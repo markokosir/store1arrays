fa7e7c35632ddfd34117a2df9d7b968bad652103
Marko Kosir <kosir.mar@gmail.com>
2018-01-11 18:19:02 +0100

    Update main.c

--- Shop/main.c
+++ Shop/main.c
@@ -2,9 +2,7 @@
 Program: Store 1 - Arrays
 Author: Marko Kosir
 
-This is a compilation of a store programs I wrote for academic purposes using three different 
-data structures from scratch. Store 1 is implemented using arrays. 
-When the program starts it allocates memory on heap and gives 100 possibly 
+Store 1 is implemented using arrays. When the program starts it allocates memory on heap and gives 100 possibly 
 entries of products.
 
 Files included:
@@ -19,17 +17,8 @@ Note: All the code is written in pure C programming language, however header fil
       inlcuded in this program(windows.h is also included for rendering reasons), which is 
       not part of ANSI C standard.
 
-This program was made entirely by Marko Kosir, it is freeware,
-feel free to use this code or change it in any way you like.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-email: kosir.mar@gmail.com
-
 Tested - Windows XP
 Compiled - Microsoft Visual Studio 2005
-		   MVS 2005 compiler gives you warnings about some of the functions and suggest that you
-		   should use ISO C++ functions and replace deprecated ones, if you want feel free to do it.
 
 Released: July 8th, 2005
 
